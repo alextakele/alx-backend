39f3c13cf99c5874694980f6b19d046a5a844881
alex221 <alextakele16@gmail.com>
2023-03-02 10:47:57 +0300

    Task 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
+"""
+Module 0-app
+"""
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    """ Greeting
-
-        Return:
-            Initial template html
-    """
-    return render_template('0-index.html')
+@app.route("/")
+def index():
+    """didplays a basic hello world message"""
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(debug=True)
