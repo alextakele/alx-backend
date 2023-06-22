cba8115b81e75ea1a3bdf2b2e5b62e110207bd85
Alexander Takele Mengesha <67500303+alexa221@users.noreply.github.com>
2023-06-22 08:58:18 -0700

    6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -14,3 +14,4 @@ const job = queue.create(queueName, data).save();
 job.on('enqueue', () =>  console.log(`Notification job created: ${job.id}`))
   .on('complete', () => console.log('Notification job completed'))
   .on('failed', () => console.log('Notification job failed'));
+
